afa8e797a87c5cb50a98495f85a3da2291b1665f
Reilly <reillyodie@gmail.com>
2021-05-21 19:32:27 -0400

    Successfully loaded  dependency packages on a new environment, created todo.txt which shows what needs to be done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -231,6 +231,8 @@ export default function Dashboard(props) {
 
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
+  console.log(recentPosts, popularPosts)
+
   return (
     <div className={classes.root}>
       <CssBaseline />
