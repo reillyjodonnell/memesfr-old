22e4d67ef7dcdf9011b83659f2da00866467dce8
Reilly <Reillyodie@gmail.com>
2021-05-27 10:59:28 -0400

    Removed dislike button

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,6 @@ import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 
 export default function Card(props) {
-  console.log(props);
   var postID = props.item.id;
   const [heart, setHeart] = useState(false);
   const [thumbUp, setThumbUp] = useState(false);
@@ -318,7 +317,7 @@ export default function Card(props) {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" />
             </svg>
-            <svg
+            {/* <svg
               onClick={currentUser ? toggleThumbDown : activatePrompt}
               xmlns="http://www.w3.org/2000/svg"
               className={thumbDown ? "active-thumbdown" : "inactive-thumbdown "}
@@ -333,7 +332,7 @@ export default function Card(props) {
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3" />
-            </svg>
+            </svg> */}
           </div>
           {options ? <OptionsExpanded /> : null}
         </div>
