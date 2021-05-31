43f2952661fdf292d1bf5b013981468f424ac8c9
Reilly <Reillyodie@gmail.com>
2021-05-31 19:02:05 -0400

    Set up firebase hosting config

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../CSS Components/ListItems.css";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
